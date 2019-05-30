6bb266c8eeb22276f392c56666d6e2367c6aa454
Milan Krstić <milan.krstic@quantox.com>
2019-05-30 15:50:13 +0200

    Paths changed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const express = require('./node_modules/express')
-const hbs = require('./node_modules/hbs')
+const express = require('express')
+const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
